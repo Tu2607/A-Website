a5cd9fec31cdcd22151300e33080dd7f7202c80f
Tu Vu <tuvu2607@gmail.com>
2020-10-14 10:57:48 -0700

    Start of weather app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api = {
+    key: "ab0d8cd95ab2e4618efe068acb334d82",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
+function App(){
+    return(
+        <div className = "app">
+            <main>
+                <div className="search-box">
+                    <input type="text" className="search-bar" placeholder="Search...">
+                    </input>
+                </div>
+            </main>
+        </div>
+    )
 }
 
 export default App;
